4effdb304ce19a1116e485aa642a7a1fd63cc3ba
Marco-rc72 <marcocauichantonio@gmail.com>
2025-08-01 19:21:34 -0500

    cambio de puerto

--- front/app/formularioContacto/page.tsx
+++ front/app/formularioContacto/page.tsx
@@ -110,7 +110,7 @@ export default function Formulario() {
     console.log("🛫 Enviando datos al backend:", formData);
 
     try {
-      const response = await fetch("http://123.456.789.123:3178/api/usuarios", {
+      const response = await fetch("http://167.172.150.250:10000//api/usuarios", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
